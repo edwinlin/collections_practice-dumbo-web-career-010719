659a1cac33f264cb9e31a36be0735f32b24ed98c
Edwin Lin <edwinlin@gmail.com>
2019-01-09 17:24:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
 
+def sort_array_asc(integers)
+  integers.sort
+end
+
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings.sort {|x,y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+  array[]
+end
+
+def reverse_array
+end
+def kesha_maker
+end 
+def find_a
+end
+def sum_array
+end
+def add_s
+end
